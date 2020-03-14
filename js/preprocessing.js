e1f7d6a1a25090ad7a3e808f50bb6c3b04b027ff
Wesley Yuen <wesleywh@students.cs.ubc.ca>
2020-03-13 18:55:51 -0700

    grouped games already released on other platform into 1 item

--- js/preprocessing.js
+++ js/preprocessing.js
@@ -2,9 +2,23 @@ d3.csv("data/games.csv").then(data => {
     function formatData() {
         const games = [];
         data.forEach(d => {
+            // Just add Sales data if game already exists on other platforms
+            const i = games.find(e => e.name == d.Name);
+            if (i != undefined) {
+                i.sales.push({
+                    sales_platform: d.Platform,
+                    na_sales: +d.NA_Sales,
+                    eu_sales: +d.EU_Sales,
+                    jp_sales: +d.JP_Sales,
+                    other_sales: +d.Other_Sales,
+                    global_sales: +d.Global_Sales
+                });
+                return;
+            }
             const game = {};
             game.name = d.Name;
             game.platform = d.Platform;
+            
             // Add platform_company column
             if (d.Platform.match(/^(PS|PS2|PS3|PS4|PSP|PSV)$/)) game.platform_company = "Sony";
             else if (d.Platform.match(/^(XB|X360|XOne)$/)) game.platform_company = "Mircosoft";
@@ -15,11 +29,14 @@ d3.csv("data/games.csv").then(data => {
             game.year = +d.Year_of_Release;
             game.genre = d.Genre;
             game.publisher = d.Publisher;
-            game.na_sales = +d.NA_Sales;
-            game.eu_sales = +d.EU_Sales;
-            game.jp_sales = +d.JP_Sales;
-            game.other_sales = +d.Other_Sales;
-            game.global_sales = +d.Global_Sales;
+            game.sales = [{
+                sales_platform: game.platform,
+                na_sales: +d.NA_Sales,
+                eu_sales: +d.EU_Sales,
+                jp_sales: +d.JP_Sales,
+                other_sales: +d.Other_Sales,
+                global_sales: +d.Global_Sales
+            }];
             games.push(game);
         });
         return games;

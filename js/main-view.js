c9a4672bbb8476bdfb4aa9b52451deb6b56c77f4
Wesley Yuen <wesleywh@students.cs.ubc.ca>
2020-03-25 01:10:29 -0700

    able to select a game and the same game released in other console_company will also be highlighted in a different color

--- js/main-view.js
+++ js/main-view.js
@@ -7,7 +7,11 @@ class MainView {
         }
         this.config.margin = _config.margin || { top: 10, bottom: 10, right: 10, left: 10 };
 
-        this.data = [];
+        this.sony_data = [];
+        this.microsoft_data = [];
+        this.nintendo_data = [];
+        this.pc_data = [];
+        this.others_data = [];
     }
 
     initVis() {
@@ -39,13 +43,7 @@ class MainView {
             .attr("y", Math.sin(2 / 5 * 2 * Math.PI) * 200 + vis.config.containerHeight / 2);
 
         vis.padding = 3; // padding within cluster
-
-        // Data
-        vis.sony_games = vis.data[0];
-        vis.microsoft_games = vis.data[1];
-        vis.nintendo_games = vis.data[2];
-        vis.pc_games = vis.data[3];
-        vis.others_games = vis.data[4];
+        vis.selectedGame = "";
 
         vis.render();
         vis.initForce();
@@ -81,54 +79,105 @@ class MainView {
             .range([15, 90]);
 
         vis.sony_circles = vis.sony_group.selectAll(".sony-nodes")
-            .data(vis.sony_games)
+            .data(vis.sony_data)
             .join("circle")
             .transition()
             .attr('class', "sony-nodes")
+            .attr('id', d => "sony" + d.index)
             .attr("r", d => vis.circleRadius(d.global_sales))
             .attr('cx', d => +vis.sony_group.attr('x') + Math.random() * vis.padding)
             .attr('cy', d => +vis.sony_group.attr('y') + Math.random() * vis.padding)
             .attr("cluster", "Sony");
 
         vis.microsoft_circles = vis.microsoft_group.selectAll(".microsoft-nodes")
-            .data(vis.microsoft_games)
+            .data(vis.microsoft_data)
             .join("circle")
             .transition()
             .attr('class', "microsoft-nodes")
+            .attr('id', d => "microsoft" + d.index)
             .attr("r", d => vis.circleRadius(d.global_sales))
             .attr('cx', d => +vis.microsoft_group.attr('x') + Math.random() * vis.padding)
             .attr('cy', d => +vis.microsoft_group.attr('y') + Math.random() * vis.padding)
             .attr("cluster", "Microsoft");
 
         vis.nintendo_circles = vis.nintendo_group.selectAll(".nintendo-nodes")
-            .data(vis.nintendo_games)
+            .data(vis.nintendo_data)
             .join("circle")
             .transition()
             .attr('class', "nintendo-nodes")
+            .attr('id', d => "nintendo" + d.index)
             .attr("r", d => vis.circleRadius(d.global_sales))
             .attr('cx', d => +vis.nintendo_group.attr('x') + Math.random() * vis.padding)
             .attr('cy', d => +vis.nintendo_group.attr('y') + Math.random() * vis.padding)
             .attr("cluster", "nintendo");
 
         vis.pc_circles = vis.pc_group.selectAll(".pc-nodes")
-            .data(vis.pc_games)
+            .data(vis.pc_data)
             .join("circle")
             .transition()
             .attr('class', "pc-nodes")
+            .attr('id', d => "pc" + d.index)
             .attr("r", d => vis.circleRadius(d.global_sales))
             .attr('cx', d => +vis.pc_group.attr('x') + Math.random() * vis.padding)
             .attr('cy', d => +vis.pc_group.attr('y') + Math.random() * vis.padding)
             .attr("cluster", "pc");
 
         vis.others_circles = vis.others_group.selectAll(".others-nodes")
-            .data(vis.others_games)
+            .data(vis.others_data)
             .join("circle")
             .transition()
             .attr('class', "others-nodes")
+            .attr('id', d => "others" + d.index)
             .attr("r", d => vis.circleRadius(d.global_sales))
             .attr('cx', d => +vis.others_group.attr('x') + Math.random() * vis.padding)
             .attr('cy', d => +vis.others_group.attr('y') + Math.random() * vis.padding)
             .attr("cluster", "others");
+
+        vis.handleSelection();
+    }
+
+    handleSelection() {
+        let vis = this;
+        vis.svg.selectAll('circle')
+            .on('click', d => {
+                console.log(d)
+                if (vis.selectedGame === "") { // if not selected, select it 
+                    const localSelected = d.console_company + d.index;
+                    d3.select("#" + localSelected)
+                        .style('stroke', '#b35227');
+                    vis.getRelatedIDs(d.name, d.console_company).forEach(d => {
+                        d3.select("#" + d)
+                            .style('stroke', '#71361c');
+                    });
+                    vis.selectedGame = localSelected;
+                } else if (vis.selectedGame === d.console_company + d.index) { // if selected, unselect it 
+                    d3.select("#" + vis.selectedGame)
+                        .style('stroke', '#ccc');
+                    vis.getRelatedIDs(d.name, d.console_company).forEach(d => {
+                        d3.select("#" + d)
+                            .style('stroke', '#ccc');
+                    });
+                    vis.selectedGame = "";
+                }
+
+            });
+    }
+
+    getRelatedIDs(name, company) {
+        let relatedIDs = [];
+        const s_result = this.sony_data.find(d => d.name === name)
+        const m_result = this.microsoft_data.find(d => d.name === name);
+        const n_result = this.nintendo_data.find(d => d.name === name);
+        const p_result = this.pc_data.find(d => d.name === name);
+        const o_result = this.others_data.find(d => d.name === name);
+
+        if (s_result !== undefined && s_result.console_company !== company) relatedIDs.push(s_result.console_company + s_result.index);
+        if (m_result !== undefined && m_result.console_company !== company) relatedIDs.push(m_result.console_company + m_result.index);
+        if (n_result !== undefined && n_result.console_company !== company) relatedIDs.push(n_result.console_company + n_result.index);
+        if (p_result !== undefined && p_result.console_company !== company) relatedIDs.push(p_result.console_company + p_result.index);
+        if (o_result !== undefined && o_result.console_company !== company) relatedIDs.push(o_result.console_company + o_result.index);
+
+        return relatedIDs;
     }
 
     // Move d to be adjacent to the cluster node.
--- js/preprocessing.js
+++ js/preprocessing.js
@@ -7,7 +7,7 @@ d3.csv("data/games.csv").then(data => {
 
     function formatDataForMain() {
         const games = [[/*Sony*/], [/*Microsoft*/], [/*Nintendo*/], [/*PC*/], [/*Others*/]];
-        data.forEach(d => {
+        data.forEach( (d, i) => {
             // update max min
             if (salesMax < +d.Global_Sales) salesMax = +d.Global_Sales;
             if (salesMin > +d.Global_Sales) salesMin = +d.Global_Sales;
@@ -23,17 +23,29 @@ d3.csv("data/games.csv").then(data => {
             game.jp_sales = +d.JP_Sales;
             game.other_sales = +d.Other_Sales;
             game.global_sales = +d.Global_Sales;
+            game.index = i;
 
             // Add platform_company column
-            if (d.Platform.match(/^(PS|PS2|PS3|PS4|PSP|PSV)$/)) games[0].push(game);
-            else if (d.Platform.match(/^(XB|X360|XOne)$/)) games[1].push(game);
-            else if (d.Platform.match(/^(3DS|DS|GB|GBA|GC|N64|NES|SNES|Wii|WiiU)$/)) games[2].push(game);
-            else if (d.Platform == "PC") games[3].push(game);
-            else games[4].push(game);
+            if (d.Platform.match(/^(PS|PS2|PS3|PS4|PSP|PSV)$/)) {
+                game.console_company = "sony"; games[0].push(game);
+            } else if (d.Platform.match(/^(XB|X360|XOne)$/)) {
+                game.console_company = "microsoft"; games[1].push(game);
+            } else if (d.Platform.match(/^(3DS|DS|GB|GBA|GC|N64|NES|SNES|Wii|WiiU)$/)) {
+                game.console_company = "nintendo"; games[2].push(game);
+            } else if (d.Platform == "PC") {
+                game.console_company = "pc"; games[3].push(game);
+            } else {
+                game.console_company = "others"; games[4].push(game);
+            }
         });
         return games;
     }
-    mainView.data = formatDataForMain();
+    const mainViewdata = formatDataForMain();
+    mainView.sony_data = mainViewdata[0];
+    mainView.microsoft_data = mainViewdata[1];
+    mainView.nintendo_data = mainViewdata[2];
+    mainView.pc_data = mainViewdata[3];
+    mainView.others_data = mainViewdata[4];
     mainView.salesMax = salesMax;
     mainView.salesMin = salesMin;
     mainView.initVis();
